bcea12ee1dd3f3fcab1dbcb00f585f677c746a33
Andrea Diaz <aidiaz5@uc.cl>
2022-01-25 22:37:13 -0300

    código completo clase 25/01/22

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,111 @@
-(() => {
+/**
+ * 2C = two of clubs (tréboles)
+ * 2D = two of diamond (diamantes)
+ * 2H = two of hearts (corazones)
+ * 2S = two of spades (espadas)
+ * 
+ * La meta es llegar a 21 pts sin pasarnos
+ */
+
+// deckOfCards = barajaDeCartas
+(() => {                                   // función anónima autoinvocada. Se conoce como el patrón de módulo en js
+
+ let deckOfCards = [];
+ // C = Tréboles, D = Diamantes, H = Corazones, S = espadas
+const types = ['C', 'D', 'H', 'S'];
+const specials = ['A', 'J', 'Q', 'K']
+
+// Score de los jugadores
+let scorePlayers = [];
+
+/* ---REFERENCIAS AL DOM, PARA QUE JS PUEDA ACCEDER AL DOM --- */
+
+// Botones de acciones
+const getBtnCard = document.querySelector("#btnGetCard");
+const getStopTurn = document.querySelector("#btnStopTurn");
+const getNewGame = document.querySelector("#btnNewGame");
+
+// Área de juego
+const divCardPlayers = document.querySelectorAll('.divCards');
+const scoreHtml = document.querySelectorAll('small');
+
+/* ---FIN DE REFERENCIAS AL DOM ---*/
+
+/** FUNCIONES */
+
+//INICIAR JUEGO
+const startGame = (numPlayer = 2) => {
+console.log('Comenzando el juego');
+// crear la baraja
+deckOfCards = createDeck();
+
+// cada nuevo juego se reinician los puntajes
+scorePlayers = [];
+for( let i=0; i<numPlayer; i++) {
+    scorePlayers.push(0);
+}
+console.log(scorePlayers);
+// puntaje de los jugadores
+
+};
+
+// CREAR BARAJA
+const createDeck = () => {
+let deckOfCards = [];
+
+for(let i=2; i <=10; i++) {
+    for(let type of types) {
+    deckOfCards.push(i + type)
+    }
+}
+
+for(let type of types) {
+    for( let special of specials) {
+        deckOfCards.push(special + type)
+    }
+}
+
+console.log(deckOfCards);
+return _.shuffle(deckOfCards);
+};
+
+// Obtener una carta (tarjeta)
+
+const getOneCard = () => {
+
+}
+
+// Valor de la carta
+const ValueCard = () => {
+
+};
+
+// Contador de puntaje
+
+const countScore = () => {
+
+}
+
+// Crear para mostrarla en el DOM
+const createCard = () => {
+
+}
+
+// Determinar un Ganador
+const winnerPlayer = () => {
+
+}
+
+// Para los turno de la computadora
+const turnComputer = () => {
+
+}
+
+/* LOS EVENTOS DE LOS BOTONES */
+
+btnNewGame.addEventListener('click', ()=>{
+    startGame();
+});
 
 })();  // función anónima autoinvocada. Se conoce como el patrón de módulo en js
+
